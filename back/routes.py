af6aa5b4d54116f50370c98b0bccfd574f0b581b
0Sendai <m.fandienko@gmail.com>
2024-10-26 05:35:39 +0300

    fix intentions get

--- back/routes.py
+++ back/routes.py
@@ -16,7 +16,7 @@ async def login(request: web.Request) -> web.Response:
     return web.json_response({'success': False})
 
 @routes.get('/admins')
-async def admins(request: web.Request) -> web.json_response:
+async def admins_get(request: web.Request) -> web.json_response:
     db = request.app[db_key]
     data = await db.con.fetch('SELECT (admin_login,is_admin) FROM admins')
     res = []
@@ -34,8 +34,16 @@ async def intentions_get(request: web.Request) -> web.json_response:
     data = await db.con.fetch(
         '''select intention, keyw from intentions right join keywords on intentions.keyword_id = keywords.id;'''
     )
-    print(data)
-    return web.json_response(data)
+    res = []
+    for i in range(len(data)):
+        tmp = dict()
+        tmp['intention'] = data[i]['intention']
+        keywords = []
+        for value in data[i]['keyw']:
+            keywords.append(value)
+        tmp['keywords'] = keywords
+        res.append(tmp)
+    return web.json_response(res)
 
 def init_app() -> web.Application:
     app = web.Application()
