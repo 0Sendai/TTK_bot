6eeb4f2c7c75d0b48b2f578b814edd78cf99781a
Lightlana <kupriyanenko-sveta@mail.ru>
2024-10-27 01:43:06 +0300

    проверь пжж

--- back/db.sql
+++ back/db.sql
@@ -26,3 +26,26 @@ create table admins(
     admin_password varchar(64),
     is_admin boolean
 );
+INSERT INTO keywords(keyw) VALUES
+('тариф', 'изменить тариф', 'тарифы', 'поменять тариф', 'тарифный план'),
+('услуга', 'дополнительная услуга', 'услуги', 'подключить услугу', 'подключить дополнительную услугу'),
+('договор', 'заключить договор', 'договоры', 'заключить контракт', 'подписать договор');
+
+INSERT INTO intentions(intention, keyword_id) VALUES
+('изменить тариф', 1),
+('подключить услугу', 2),
+('заключить договор', 3);
+
+INSERT INTO additional_text(add_text, intention_id) VALUES
+('Для изменения тарифа пожалуйста, укажите желаемый тарифный план.', 1),
+('Для подключения дополнительной услуги пожалуйста, укажите название услуги.', 2),
+('Для заключения договора пожалуйста, укажите детали договора.', 3);
+
+INSERT INTO users(account) VALUES
+(123456),
+(654321);
+
+INSERT INTO admins(admin_login, admin_password, is_admin) VALUES
+('admin1', 'password1', true),
+('admin2', 'password2', true),
+('editor1', 'password3', false);
